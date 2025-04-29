0e71fc75403f2b5d6f7fc059de74d5e1bdc7f0e4
Samadhi Ayodhya Gamaarachchi <samadhigamaarachchi07@gmail.com>
2025-04-30 00:37:15 +0530

    create file structure

--- SpendSmart_Backend/Data/ApplicationDbContext.cs
+++ SpendSmart_Backend/Data/ApplicationDbContext.cs
@@ -1,6 +1,18 @@
-﻿namespace SpendSmart_Backend.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpendSmart_Backend.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext:DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+     
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Add any additional configuration here
+        }
     }
+    
 }
