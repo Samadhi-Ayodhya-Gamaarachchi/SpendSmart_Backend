f8bd52dbcedb96277c14a1872aaf96f81da36212
behan02 <behancrick@gmail.com>
2025-04-30 11:40:33 +0530

    Add the models

--- SpendSmart_Backend/Data/ApplicationDbContext.cs
+++ SpendSmart_Backend/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SpendSmart_Backend.Models;
 
 namespace SpendSmart_Backend.Data
 {
@@ -7,12 +8,24 @@ namespace SpendSmart_Backend.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
-     
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UserAdmin>()
+                .HasKey(ua => new { ua.UserId, ua.ManagerId });
             // Add any additional configuration here
         }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<UserAdmin> UserAdmins { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Budget> Budgets { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Goal> Goals { get; set; }
+        public DbSet<Report> Reports { get; set; }
+
+        
     }
     
 }
--- SpendSmart_Backend/Models/Budget.cs
+++ SpendSmart_Backend/Models/Budget.cs
@@ -3,23 +3,22 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SpendSmart_Backend.Models
 {
-    public class Transaction
+    public class Budget
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string Type { get; set; }
-        public int CategoryId { get; set; }
-        public decimal Amount { get; set; }
-        public DateTime Date { get; set; }
-        public string? Description { get; set; }
+        public DateTime MonthYear { get; set; }
+        public decimal AllocatedAmount { get; set; }
+        public decimal SpendAmount { get; set; }
+        public string Description { get; set; }
         public int UserId { get; set; }
-
-        [ForeignKey("CategoryId")]
-        public Category Category { get; set; }
+        public int CategoryId { get; set; }
 
         [ForeignKey("UserId")]
         public User User { get; set; }
 
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
     }
 }
