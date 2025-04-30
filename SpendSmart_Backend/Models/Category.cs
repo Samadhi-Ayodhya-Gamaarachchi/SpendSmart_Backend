afe1b644b0989cb4c43faada09e4766e9afdfe8c
behan02 <behancrick@gmail.com>
2025-04-30 09:17:33 +0530

    Add Transaction.cs and Category.cs files

---
+++ SpendSmart_Backend/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpendSmart_Backend.Models
+{
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Transaction> Transactions { get; set; }
+    }
+}
