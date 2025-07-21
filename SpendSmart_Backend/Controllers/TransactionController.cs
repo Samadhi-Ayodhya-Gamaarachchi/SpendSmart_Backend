d822c8f999329c00ac5963f58d87e1d5efc67e66
Samadhi Ayodhya Gamaarachchi <samadhigamaarachchi07@gmail.com>
2025-07-22 00:00:10 +0530

    add backend authentication token pass

--- SpendSmart_Backend/Controllers/TransactionController.cs
+++ SpendSmart_Backend/Controllers/TransactionController.cs
@@ -6,7 +6,7 @@ using SpendSmart_Backend.Models;
 using SpendSmart_Backend.Services;
 
 namespace SpendSmart_Backend.Controllers
-{
+{   
     [ApiController]
     [Route("api/[controller]")]
     public class TransactionController : Controller
